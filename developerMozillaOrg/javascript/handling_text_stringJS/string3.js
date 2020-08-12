1c6000a0ee1b58856d242b8949e02ca7f506124a
STAR <sil.mar.luiz1@gmail.com>
2020-08-12 10:41:31 -0300

    "solution string3  on  handling_text"

--- developerMozillaOrg/javascript/handling_text_stringJS/string3.js
+++ developerMozillaOrg/javascript/handling_text_stringJS/string3.js
@@ -8,3 +8,22 @@ Store the new quote in a variable called fixedQuote.
 3. There is one more small fix to do — add a full stop onto the end of the quote,
 and save the final version in a variable called finalQuote.
 */
+
+let quote = 'I dO nOT lIke gREen eGgS anD HAM';
+
+// Add your code here
+let lowerQuote = quote.toLowerCase();
+let fixedQuote = lowerQuote.toUpperCase(0) + lowerQuote.slice(1);
+let finalQuote = fixedQuote.replace('green eggs and ham','crabs and muzzarella'+ '.');
+
+// Don't edit the code below here!
+
+section.innerHTML = ' ';
+let para1 = document.createElement('p');
+para1.textContent = fixedQuote;
+let para2 = document.createElement('p');
+para2.textContent = finalQuote;
+
+section.appendChild(para1);
+section.appendChild(para2);
+    
