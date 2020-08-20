e2fa6f0b29b299b707900f63c515ab1a585fcc80
STAR <sil.mar.luiz1@gmail.com>
2020-08-20 10:17:42 -0300

    "definição de funcionalidade e das variáveis"

---
+++ developerMozillaOrg/javascript/Projects/ChangeCase/changeCase.js
@@ -0,0 +1,16 @@
+/* Criar uma programa que converta string de maiúsculo para minúsculo
+1. Converter todo o texto para maiúsculo;
+2. Coverter todo o texto para minúsculo;
+3. Converter apenas o primeiro caracter da string de cada frase;
+4. Converter o texto alternar os caracteres entre maiúculo e minúsculo;
+5. Converter o texto alternar os caracteres entre minúsculo e maiúsculo;
+*/
+
+let enterText = prompt('');
+let texToAllUpper;
+let textToallLower;
+let textFirstUpper;
+let textFirstFrase;
+let textAlterUpperLower;
+let textAlterLowerUpper;
+let stratChange;
